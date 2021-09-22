3e440a6159862a69a1eca810a3d06490de7be410
ozymohliad <olexandrzymohliad@gmail.com>
2021-09-22 20:04:43 +0300

    vim emacs update

--- emacs/init.el
+++ emacs/init.el
@@ -1,25 +1,46 @@
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(menu-bar-mode -1)
+(set-face-attribute 'default nil :font "IBM Plex Mono" :height 110)
 
-(set-fringe-mode 10)
-(setq visible-bell t)
-(load-theme 'wombat)
+(defun edit-config ()
+  "Open my config"
+  (interactive)
+  (find-file user-init-file))
+(global-set-key (kbd "C-c c") 'edit-config)
 
-;; initialize package sources
+(setq echo-keystrokes 0.1) ; decrease delay between keypress and echo
+(setq inhibit-startup-message t) ; start with a clean screen
+(scroll-bar-mode 0) ; disable scrollbar
+(tool-bar-mode 0) ; disable toolbar
+(tooltip-mode 0) ; disable tooltips
+(menu-bar-mode 0) ; disable menubar
+(load-theme 'misterioso) ; color scheme
+(set-cursor-color "#aaaaaa") ; cursor color
+
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("melpa-stable" . "https://stable.melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
+(require 'smooth-scrolling)
+(require 'smooth-scroll)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(restclient geiser-mit use-package smooth-scrolling smooth-scroll)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(smooth-scrolling-mode t) ; scroll screen when cursor is nearits edge
