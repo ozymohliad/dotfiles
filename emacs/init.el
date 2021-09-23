6a923e86e87379933962bebc6cbca7e82778cddd
ozymohliad <olexandrzymohliad@gmail.com>
2021-09-23 09:55:52 +0300

    emacs

--- emacs/init.el
+++ emacs/init.el
@@ -12,7 +12,7 @@
 (tool-bar-mode 0) ; disable toolbar
 (tooltip-mode 0) ; disable tooltips
 (menu-bar-mode 0) ; disable menubar
-(load-theme 'misterioso) ; color scheme
+(load-theme 'wombat) ; color scheme
 (set-cursor-color "#aaaaaa") ; cursor color
 
 (require 'package)
@@ -27,8 +27,8 @@
   (package-install 'use-package))
 
 (require 'smooth-scrolling)
-(require 'smooth-scroll)
-
+(require 'evil)
+(evil-mode 1)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -36,7 +36,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(restclient geiser-mit use-package smooth-scrolling smooth-scroll)))
+   '(lsp-ui lsp-java dirtree doom-modeline counsel evil restclient geiser-mit use-package smooth-scrolling smooth-scroll)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -44,3 +44,31 @@
  ;; If there is more than one, they won't work right.
  )
 (smooth-scrolling-mode t) ; scroll screen when cursor is nearits edge
+
+(use-package ivy
+  :diminish ivy-mode
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill)))
+(ivy-mode 1)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+(require 'lsp-java)
+(use-package lsp-ui)
+(add-hook 'java-mode-hook #'lsp)
